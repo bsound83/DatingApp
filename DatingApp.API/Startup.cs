611c034d7c13084a153b090ab7408ec7bd089cbc
Bing Soundara <bing.soundara@perficient.com>
2020-04-16 12:53:51 -0500

    Pre-publishing

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,10 +31,28 @@ namespace DatingApp.API
         }
                 public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            ConfigureServices(services);
+        }
+
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => {
+                x.UseLazyLoadingProxies();
+                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            ConfigureServices(services);
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));            
+            
             services.AddControllers().AddNewtonsoftJson(opt => 
             {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
@@ -85,17 +103,21 @@ namespace DatingApp.API
 
             app.UseRouting();
             
-            String[] origins = new String[]{"http://localhost:4200"};
-            app.UseCors(x => x.WithOrigins(origins).AllowAnyMethod().AllowAnyHeader());
-            //app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
             app.UseAuthentication();
             
             app.UseAuthorization();
+            
+            String[] origins = new String[]{"http://localhost:4200"};
+            app.UseCors(x => x.WithOrigins(origins).AllowAnyMethod().AllowAnyHeader());
+            //app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());            
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
